c87a94cc76cf128801e4cf5e79925576d0e1e2ec
A Anand <a.anand.91119@gmail.com>
2024-11-27 19:56:29 +0530

    Had to remove and install nix again from scratch

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -5,27 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    # Homebrew
-    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    # Declarative tap management
-    # homebrew-core = {
-    #   url = "github:homebrew/homebrew-core";
-    #   flake = false;
-    # };
-    # homebrew-cask = {
-    #   url = "github:homebrew/homebrew-cask";
-    #   flake = false;
-    # };
-    # homebrew-bundle = {
-    #   url = "github:homebrew/homebrew-bundle";
-    #   flake = false;
-    # };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, 
-  # nix-homebrew,
-  # homebrew-core, homebrew-cask, homebrew-bundle 
-   }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -73,41 +55,6 @@
         '';
       };
 
-       homebrew = {
-         enable = true;
-         casks = [
-            "the-unarchiver"
-            "alacritty"
-            "font-meslo-lg-nerd-font"
-         ];
-         brews = [
-#            # `brew install --with-rmtp`, `brew services restart` on version changes
-#            {
-#                name = "denji/nginx/nginx-full";
-#                args = [ "with-rmtp" ];
-#                restart_service = "changed";
-#            }
-#            # `brew install`, always `brew services restart`, `brew link`, `brew unlink mysql` (if it is installed)
-#            {
-#                name = "mysql@5.6";
-#                restart_service = true;
-#                link = true;
-#                conflicts_with = [ "mysql" ];
-#            }
-         ];
-         onActivation.cleanup = "zap";
-       };
-
-      # Allow install of non open-source apps
-      nixpkgs.config.allowUnfree = true;
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
       # Custom aliases in the shell environment
       environment.shellAliases = {
         # better ls
@@ -146,9 +93,22 @@
         };
         shellInit = ''
           # Homebrew setup
-          eval "$(/opt/homebrew/bin/brew shellenv)"
+          # eval "$(/opt/homebrew/bin/brew shellenv)"
         '';
       };
+
+      # Allow install of non open-source apps
+      nixpkgs.config.allowUnfree = true;
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -157,9 +117,7 @@
       # $ darwin-rebuild changelog
       system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
+      # Default system settings
       system.defaults = {
         dock.autohide = true;
         dock.mru-spaces = true; # Most Recently Used spaces.
@@ -171,9 +129,7 @@
 
       # Enable sudo authentication with Touch ID.
       security.pam.enableSudoTouchIdAuth = true;
-      # Log file for nix-daemon
-      services.nix-daemon.logFile = "/var/log/nix-daemon.log";
-      
+
       # Nix-darwin does not link installed applications to the user environment. This means apps will not show up
       # in spotlight, and when launched through the dock they come with a terminal window. This is a workaround.
       # Upstream issue: https://github.com/LnL7/nix-darwin/issues/214
@@ -185,50 +141,29 @@
         };
       in
         pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read -r src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-            '';
-
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+      
     };
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#
+    # $ darwin-rebuild build --flake .#Anands-MacBook-Pro--M3-Pro
     darwinConfigurations."Anands-MacBook-Pro--M3-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ 
-        configuration 
-        # nix-homebrew.darwinModules.nix-homebrew
-        # {
-        #   nix-homebrew = {
-        #     # Install Homebrew under the default prefix
-        #     enable = true;
-        #     # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-        #     enableRosetta = true;
-        #     # User owning the Homebrew prefix
-        #     user = "aanand";
-        #     # # Declarative tap management
-        #     # taps = {
-        #     #   "homebrew/homebrew-core" = homebrew-core;
-        #     #   "homebrew/homebrew-cask" = homebrew-cask;
-        #     #   "homebrew/homebrew-bundle" = homebrew-bundle;
-        #     # };
-        #     # # Enable fully-declarative tap management
-        #     # # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-        #     # mutableTaps = false;
-        #   };
-        # }
-      ];
+      modules = [ configuration ];
     };
 
     # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."Anands-MacBook-Pro--M3-Pro".pkgs;
+
   };
 }
