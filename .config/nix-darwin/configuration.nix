b2a399426d433a148f4caa0926e946ce0e84b217
A Anand <a.anand.91119@gmail.com>
2024-11-28 14:09:16 +0530

    Nix flake refactoring and adding home-manager WIP

--- .config/nix-darwin/configuration.nix
+++ .config/nix-darwin/configuration.nix
@@ -124,9 +124,6 @@
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
 
-  # Set Git commit hash for darwin-version.
-  system.configurationRevision = self.rev or self.dirtyRev or null;
-
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
 
--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -31,6 +31,10 @@
       system = "aarch64-darwin";
       modules = [
         ./configuration.nix
+        {
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+        }
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
