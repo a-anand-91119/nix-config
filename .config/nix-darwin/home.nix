ed5dbb613ce70b4e366df02aca5728bce77ea595
A Anand <a.anand.91119@gmail.com>
2024-11-29 22:45:25 +0530

    git config generated using home-manager

--- .config/nix-darwin/home.nix
+++ .config/nix-darwin/home.nix
@@ -65,4 +65,30 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userEmail = "a.anand.91119@gmail.com";
+    userName = "A Anand";
+    delta.enable = true;
+    delta.options = {
+      decorations = {
+        commit-decoration-style = "bold yellow box ul";
+        file-decoration-style = "none";
+        file-style = "bold yellow ul";
+        navigate = true;
+        side-by-side = true;
+      };
+      features = "decorations";
+      whitespace-error-style = "22 reverse";
+    };
+    extraConfig = {
+      merge = {
+        conflictstyle = "diff3";
+      };
+      diff = {
+        colorMoved = "default";
+      };
+    };
+  };
 }
