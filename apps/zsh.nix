c11f92daa41da0f62514c19c61c4c7dd7866c200
A Anand <a.anand.91119@gmail.com>
2025-04-21 19:53:40 +0530

    zprof settings, disabled by default

--- apps/zsh.nix
+++ apps/zsh.nix
@@ -95,29 +95,21 @@
 
       ### ------------ FZF ------------- ###
 
-      # >>> conda initialize >>>
-      # !! Contents within this block are managed by 'conda init' !!
-      __conda_setup="$('/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-      if [ $? -eq 0 ]; then
-          eval "$__conda_setup"
-      else
-          if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-              . "/opt/anaconda3/etc/profile.d/conda.sh"
-          else
-              export PATH="/opt/anaconda3/bin:$PATH"
-          fi
-      fi
-      unset __conda_setup
-      # <<< conda initialize <<<
-
       ### ------------ jenv ------------- ###
       export PATH="$HOME/.jenv/bin:$PATH"
       # jenv add $HOMEBREW_PREFIX/opt/openjdk@23
       eval "$(jenv init - --no-rehash)"
       ### ------------ jenv ------------- ###
+
+      # echo "--- Zsh Profiling Results ---"
+      # zprof
+      # echo "-----------------------------"
+
     '';
 
     initExtraFirst = ''
+    # zmodload zsh/zprof
+
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
       # confirmations, etc.) must go above this block; everything else may go below.
