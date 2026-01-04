cb42bd543bb6c7c1e332a89f8d0385885f44fcc1
A Anand <a.anand.91119@gmail.com>
2026-01-04 10:11:52 +0530

    added more brew packages and commented out masapps

--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -30,21 +30,23 @@
       "opentofu"
       "kubeseal"
       "maven"
+      "opencode"
     ];
     taps = [
       "homebrew/homebrew-cask"
       "homebrew/homebrew-core"
     ];
     masApps = {
-      Amphetamine = 937984704;
-      Tailscale = 1475387142;
-      LocalSend = 1661733229;
-      WhatsApp = 310633997;
-      Telegram = 747648890;
+#      Amphetamine = 937984704;
+#      Tailscale = 1475387142;
+#      LocalSend = 1661733229;
+#      WhatsApp = 310633997;
+#      Telegram = 747648890;
     };
     casks = [
       "the-unarchiver"
       "alacritty"
+      "freelens"
       "font-meslo-lg-nerd-font"
       "font-monaspace-nerd-font"
       "font-noto-sans-symbols-2"
